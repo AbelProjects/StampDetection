f46bc209df6e75bf7d0375f135d3a158b02b3938
Nikita Shishkin <nikitashn@icloud.com>
2023-01-15 19:47:25 +0300

    add flask files

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import pandas as pd
+from stamp_engine import FolderEngine
+import zipfile
+import os
+from pathlib import Path
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET','POST'])  
+def main():  
+    return render_template("index.html")    
+
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename)
+        data = zipfile.ZipFile(f'{f.filename}', 'r')
+        data.extractall()
+        relative_folder_path = f'./{f.filename.split(".")[0]}'
+        folder_path = os.path.abspath(relative_folder_path)
+        engine = FolderEngine(folder_path)
+        engine.make_stamp_folders(folders_path=folder_path)
+        return render_template("index.html", output = f'Файл обработан')  
+
+# Running the app
+if __name__ == '__main__':
+    app.run(debug = False)
